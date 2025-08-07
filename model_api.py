c3a08ad388b1ac2ff01a7dd3222f2d49e248d023
Your Name <akashkrishh75@gmail.com>
2025-08-07 20:36:18 +0530

    updated

--- model_api.py
+++ model_api.py
@@ -25,4 +25,7 @@ def generate_roadmap():
         return jsonify({"error": "Something went wrong. Try again later."}), 500
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
