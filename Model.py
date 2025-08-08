e147b68d55a40368d3872cc6ebd7429d0480cea1
Your Name <akashkrishh75@gmail.com>
2025-08-08 18:34:47 +0530

    day 02

--- Model.py
+++ Model.py
@@ -18,7 +18,7 @@ def get_model():
     global _model
     if _model is None:
         from sentence_transformers import SentenceTransformer
-        _model = SentenceTransformer('paraphrase-MiniLM-L3-v2')
+        _model = SentenceTransformer('all-MiniLM-L6-v2')
     return _model
 
 # === Utility Functions ===
